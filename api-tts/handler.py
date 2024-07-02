6c828d7e311f28e90f90c4317e9a7e8fd0a17118
daniel-compasso <76558798+daniel-compasso@users.noreply.github.com>
2024-07-02 12:44:11 -0300

    :tada: init: evaluation launching.

---
+++ api-tts/handler.py
@@ -0,0 +1,21 @@
+import json
+
+
+def health(event, context):
+    body = {
+        "message": "Go Serverless v3.0! Your function executed successfully!",
+        "input": event,
+    }
+
+    response = {"statusCode": 200, "body": json.dumps(body)}
+
+    return response
+
+def v1_description(event, context):
+    body = {
+        "message": "TTS api version 1."
+    }
+
+    response = {"statusCode": 200, "body": json.dumps(body)}
+
+    return response
