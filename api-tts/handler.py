0a48b54078f97a915526a7d3f1b22ccaf0e3af80
Rafael Rezende <rafaelrez2004@gmail.com>
2024-07-03 16:17:14 -0300

    :bulb: docs: comentando parte da primeira API

--- api-tts/handler.py
+++ api-tts/handler.py
@@ -4,7 +4,7 @@ import json
 def health(event, context):
     body = {
         "message": "Go Serverless v3.0! Your function executed successfully!",
-        "input": event,
+        #"input": event,
     }
 
     response = {"statusCode": 200, "body": json.dumps(body)}
