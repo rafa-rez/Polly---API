47df2bc6100df7070b038ce8f28e83db2e8bd1ef
Rafael Rezende <rafaelrez2004@gmail.com>
2024-07-04 13:52:39 -0300

    :sparkles: feat: adicionando ID para cada frase gerada.

--- api-tts/handler.py
+++ api-tts/handler.py
@@ -1,5 +1,6 @@
 import json
-
+import random
+import string
 
 def health(event, context):
     body = {
@@ -20,17 +21,20 @@ def v1_description(event, context):
 
     return response
 
+def generate_id(length=5):
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+
 def post(event, context):
     body = json.loads(event.get('body', '{}'))
     
     phrase = body.get('phrase', '')
+    phrase_id = generate_id()
 
     response_body = {
+        "id": phrase_id,
         "message": f"Received phrase: {phrase}"
     }
 
-    response = {"statusCode": 200,"body": json.dumps(response_body),"headers": {"Content-Type": "application/json"}
-    }
+    response = {"statusCode": 200, "body": json.dumps(response_body), "headers": {"Content-Type": "application/json"}}
 
     return response
-    
